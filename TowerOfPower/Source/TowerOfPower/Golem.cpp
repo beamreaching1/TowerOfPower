4ec3da99fcacf38779dc4b92c4c1351ca7d0d549
beamreaching1 <caydenw@g.clemson.edu>
2021-04-17 21:06:38 -0400

    Final Update

--- TowerOfPower/Source/TowerOfPower/Golem.cpp
+++ TowerOfPower/Source/TowerOfPower/Golem.cpp
@@ -47,10 +47,6 @@ void AGolem::BeginPlay() {
 	
 	check(GEngine != nullptr);
 
-	// Display a debug message for five seconds. 
-	// The -1 "Key" value argument prevents the message from being updated or refreshed.
-	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
-
 }
 
 // Called every frame
